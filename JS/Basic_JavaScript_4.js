e36dce4b04cb278ac4edababc0d0e5764fac712a
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-07-17 10:54:10 -1000

    inital commit of js4

---
+++ JS/Basic_JavaScript_4.js
@@ -0,0 +1,12 @@
+function Dictonary_1() {
+    var Camaro = {
+    Make:"Chevrolet",
+    Model:"Camaro",
+    Year:"2019",
+    Transmission:"Manual",
+    Engine:"V8",
+    Engine:"V6"
+    };
+    delete Camaro.Year
+    document.getElementById("dictionary").innerHTML = Camaro.Year;
+}
